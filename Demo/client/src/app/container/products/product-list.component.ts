dc04de4abb0f756a38584c9cc7d59875675ff5a7
junreturn1982 <ngochoang071982@gmail.com>
2017-07-06 16:10:19 +0700

    encapsule component style

--- Demo/client/src/app/container/products/product-list.component.ts
+++ Demo/client/src/app/container/products/product-list.component.ts
@@ -9,7 +9,8 @@ import { IProduct } from './product';
 @Component({
     // pm project manager
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
     pageTitle: string = "Product List";
