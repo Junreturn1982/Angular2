a34e4a27a07aaed9dc32884bb55f17936680c58d
junreturn1982 <ngochoang071982@gmail.com>
2017-07-07 23:24:36 +0700

    product detail finish

--- Demo/client/src/app/container/products/product-detail.component.ts
+++ Demo/client/src/app/container/products/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { Subscription }       from 'rxjs/Subscription';
+
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
@@ -12,18 +14,35 @@ export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product: IProduct;
     errorMessage: string;
+    private sub: Subscription;
     // the activated route instance is then  injected 
     // into this component class.
-    constructor(private _route: ActivatedRoute, 
-                private _router: Router) {
-
+    constructor(private _route: ActivatedRoute,
+                private _router: Router,
+                private _productService: ProductService) {
     }
     ngOnInit(): void {
-        let id = +this._route.snapshot.params['id'];
-        this.pageTitle += `: ${id}`;
+        this.sub = this._route.params.subscribe(
+            params => {
+                let id = +params['id'];
+                this.getProduct(id);
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    getProduct(id: number) {
+        this._productService.getProduct(id).subscribe(
+            product => this.product = product,
+            error => this.errorMessage = <any>error);
     }
 
     onBack(): void {
         this._router.navigate(['/products']);
+    }
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product Detail: ' + message;
     }
 }
