275dfe9b4d628c6d0e2ab9f54985c81c648ebc4b
junreturn1982 <ngochoang071982@gmail.com>
2017-07-07 11:06:26 +0700

    registering the service

--- Demo/client/src/app/app.component.ts
+++ Demo/client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductService } from './container/products/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ ProductService ]
 })
 export class AppComponent {
   title = 'Product Management';
